41c33e2817169d65bfaa507f70683ee999be1f72
Mikezhiwei <lzw00000@qq.com>
2013-09-28 01:29:36 +0800

    学生管理系统java版（链表）

---
+++ Subject.java
@@ -0,0 +1,10 @@
+package studennode;
+
+public class Subject {
+           int  cgrade=-1;
+           int  cdoubleaddgrade=1;
+           int  javagrade=-1;
+           int  datastrugrade=-1;
+           
+	
+}
